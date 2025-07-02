d6d36596afe1fa85dadf135e99d76981314b9cea
Matilde Valente <matvalente@gmail.com>
2025-07-02 16:23:27 +0100

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default ({ mode }: { mode: string }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return defineConfig({
-    base: 'https://luisalino.github.io/website-portfolio/',
+    base: '/website-portfolio/',
     plugins: [react(), tailwindcss()],
     server: {
       cors: true,
